9c1d825110e706b0f2832c2ad07af356d003c954
aleontiev <alonetiev@gmail.com>
2012-11-06 16:30:16 -0500

    Update chrome/initialize.js

--- chrome/initialize.js
+++ chrome/initialize.js
@@ -1,23 +1,6 @@
 (function(){
-    var TIMEOUT = 500;
-    function load(){
-        // try to get the TT user ID
-        var id = tryGetUserID();
-        if (id === null){
-            // set a timer to try again
-            setTimeout(function(){ load(); },TIMEOUT);
-        }
-        else{
-            // get the TTX script from server
-            var TTX = document.createElement('script');
-            TTX.setAttribute('src','http://turntablex.com/plugin.php?id='+id);
-            TTX.setAttribute('type','text/javascript');
-            (document.body || document.head).appendChild(TTX);
-        }
-    }
-    function tryGetUserID(){
-        if (typeof(turntable) === 'undefined')
-            return null;
-        return turntable.user.id;
-    }
+    var TTX = document.createElement('script');
+    TTX.setAttribute('src','http://turntablex.com/plugin.php?id='+id);
+    TTX.setAttribute('type','text/javascript');
+    (document.body || document.head).appendChild(TTX);
 })();
