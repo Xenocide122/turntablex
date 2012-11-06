1cfe4eaaa5fee275fb95806697124563358ff224
aleontiev <alonetiev@gmail.com>
2012-11-06 14:20:12 -0500

    Update ttx.js

--- ttx.js
+++ ttx.js
@@ -19,27 +19,35 @@ window.TTX = null;
         
         function updateRoomInfo(callback){
             _room = null;
-            _location = window.location.pathname;    
+            _id = null;
+            _location = window.location.pathname;  
+            
             for (var o in _turntable){
                 if (_turntable[o] !== null && _turntable[o].creatorId){
                     _room = _turntable[o];
-                    log('Entering room ' + _location + ':');
-                    log(_room);
+                    log('Entering room ' + _location);
                     _mods = _room.moderators || [];
-                    log(_mods);
-                    break;
-                }
-            }
-            if (_room){
-                for (var o in _room){
-                    if(_room[o] !== null && _room[o].myuserid){
-                        _manager = _room[o];
-                        log('Found room manager:');
-                        log(_manager);
+                    if (_id){
                         break;
                     }
                 }
-                callback(); // success
+            }
+            if (_room){ // found turntable data
+                for (var o in _room){
+                    if(_room[o] !== null && _room[o].myuserid){
+                        _manager = _room[o];
+                        _id = manager.myuserid;
+                        log('Found room manager.');
+                        break;
+                    }
+                }
+                if (_id){
+                    callback(); // success
+                }
+                else{
+                    // try again
+                    setTimeout(function(){ updateRoomInfo(callback); }, 250);
+                }
             }
             else{
                 // try again
