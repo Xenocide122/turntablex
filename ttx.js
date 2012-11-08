99b05695c56b91422eb419f7e23db7ed2ce31a29
aleontiev <alonetiev@gmail.com>
2012-11-08 17:43:58 -0500

    Update ttx.js

--- ttx.js
+++ ttx.js
@@ -45,13 +45,13 @@ window.TTX = null;
 	function updateHeader(){
 		var header = $('.room .name');
 		var song_bar = header.find('#ttx_songbar');
-		var text = 'Now playing: '+_currentSong.title+' by <b>'+_currentSong.artist+'</b> (' + _currentSong.upvotes + '<span style="color:#00ff11">&#9650;</span>,' + _currentSong.downvotes + '&#9660,'+_currentSong.hearts+'<span style="color:#f00">&#10084;</span>';
+		var text = 'Now playing: '+_currentSong.title+' by <b>'+_currentSong.artist+'</b> (' + _currentSong.upvotes + '<span style="color:#00ff00">&#9650;</span>,' + _currentSong.downvotes + '&#9660,'+_currentSong.hearts+'<span style="color:#f00">&#10084;</span>';
 		if (song_bar.length){
 			song_bar.text(text);
 		}
 		else{
 			header.text(header.text()+': ');
-			$('<span class="#ttx_songbar" style="font-size:12px; font-weight:normal">' + text + '</span>').appendTo(header);
+			$('<span class="#ttx_songbar" style="font-size:9px; font-weight:normal">' + text + '</span>').appendTo(header);
 		}
 	}
 	// reset the state of the room
