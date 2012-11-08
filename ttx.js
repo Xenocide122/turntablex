92b092d4bbb0189e69bb6485f7bb16d73eb679e7
aleontiev <alonetiev@gmail.com>
2012-11-08 13:42:06 -0500

    Update ttx.js

--- ttx.js
+++ ttx.js
@@ -135,7 +135,7 @@ window.TTX = null;
 	    chat.find('.guestListIcon').hide();
 	    chat.find('.chatResizeIcon').hide();
 	    
-	    room_info.find('.content').css({left:0,top:-1*stage_height,height:stage_height});
+	    room_info.find('.content').css({left:0,top:-1*(10+stage_height),height:(10+stage_height)});
 	    room_info.find('.songlong').css({height:500});
 	    room_info.find('.button').css({left:125}).unbind('click').bind('click',function(){ 
  	    	var direction = 1;
