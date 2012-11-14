4ee9cc5a4d618454ebcbd53f49692457efb53b0d
aleontiev <alonetiev@gmail.com>
2012-11-13 20:56:10 -0500

    Update ttx.js

--- ttx.js
+++ ttx.js
@@ -480,7 +480,7 @@ window.TTX = null;
 						<div><div style="display:inline-block; margin: 8px; width:80px">Text:</div><input style="width: 300px; height:10px; position:relative; top: 9px; margin-right:10px" id="ttxLaptopText" type="text" value="'+newLaptopAnimation.text.display+'"/>tick number: <input type="text" id="ttxLaptopTicks" style="width:30px;height:10px;position:relative;top:9px;" value="'+ newLaptopAnimation.text.tick +'"/></div>\
 						<div><div style="display:inline-block; margin: 8px; width:80px">Colors:</div><input style="width: 300px; height:10px; position:relative; top: 9px; margin-right:10px" id="ttxLaptopColors" type="text" value="'+newLaptopAnimation.text.colors+'"/>each letter: <input type="checkbox" id="ttxLaptopColorEach" '+ (newLaptopAnimation.text.colorEachLetter ? 'checked="checked"':'') + '</div>\
 						</div>');
-				$('#ttxLaptopTextSettings').after('<div id="ttxLaptopButtons" style="text-align:center; width:100%; margin-top: 5px;"\
+				$('#ttxLaptopTextSettings').after('<div id="ttxLaptopButtons" style="text-align:center; width:100%; margin-top: 5px;">\
 									<button id="ttxLaptopPreview" class="submit">Preview</button>\
 									<button id="ttxLaptopSave" class="submit">Save</button>\
 								   </div>');
