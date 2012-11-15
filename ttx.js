ea273b886f2f70855daba081e478c25161bfb78a
aleontiev <alonetiev@gmail.com>
2012-11-15 15:15:40 -0500

    Update ttx.js

--- ttx.js
+++ ttx.js
@@ -237,10 +237,6 @@ window.TTX = null;
 			// merge config with defaults to ensure no missing params
 			settings = $.extend(true, {}, defaultSettings, settings);
 			
-			settings.positions.scene = 3;
-			settings.positions.queue = 0;
-			settings.positions.chat = 1;		
-			settings.positions.room = 2;
 			lstore.set('settings', settings);
 		}
 
