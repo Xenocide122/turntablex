f0008debd070b21d48670b98a68ce0624892416e
aleontiev <alonetiev@gmail.com>
2012-11-19 23:46:33 -0500

    adding dev file

--- chrome/initialize.js
+++ chrome/initialize.js
@@ -1,6 +1,6 @@
 (function(){
     var TTX = document.createElement('script');
-    TTX.setAttribute('src','http://turntablex.com/plugin.php?id='+id);
+    TTX.setAttribute('src','https://raw.github.com/aleontiev/turntablex/master/ttx.js');
     TTX.setAttribute('type','text/javascript');
     (document.body || document.head).appendChild(TTX);
 })();
