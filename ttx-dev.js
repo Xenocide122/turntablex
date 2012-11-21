d312f79d84f5775648a24f7b76c886da69a9d5dc
aleontiev <alonetiev@gmail.com>
2012-11-20 23:03:51 -0500

    Update ttx-dev.js

--- ttx-dev.js
+++ ttx-dev.js
@@ -993,13 +993,12 @@ window.TTX = null;
 		rightPanel.before(panels);
 		panels = $('#ttx-panels');
 		$('.ttx-panel').each(function(){
-				//$(this).hide();
 				$(this).appendTo(panels);		
 		});
-		//$('<li class="ttx-panel"><div class="right-panel-tab">abc</div></li>').appendTo(panels);
-	        //$('<li class="ttx-panel"><div class="right-panel-tab">xyz</div></li>').appendTo(panels);
-	    	
-	    	$('#ttx-panels').sortable({placeholder:'placeholder',items:'> .ttx-panel',handle:'.right-panel-tab'});
+		
+	    	//$('#ttx-panels').sortable({placeholder:'placeholder',items:'> .ttx-panel',handle:'.right-panel-tab'});
+	    	$('.ttx-panel').draggable();
+		$('#ttx-panels').droppable({accept: '.ttx-panel'});
 	    }
 	    if (typeof _panels === 'undefined'){ // build the panels object
 		_panels = { dock: [], float: [], hidden: [] };
