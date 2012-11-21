419692ca8561d55b8a381ee94eb81f679772ce9f
aleontiev <alonetiev@gmail.com>
2012-11-21 11:28:20 -0500

    Update ttx-dev.js

--- ttx-dev.js
+++ ttx-dev.js
@@ -821,7 +821,7 @@ window.TTX = null;
 						extrasClass = extrasClass + ' isMod';
 						icons = icons + ICONS.mod;
 					}
-					if ($name.hasClass('superuser')){
+					if ($name.find('.icons').hasClass('superuser')){
 						extrasClass = extrasClass + ' isSuper';
 						icons = icons + ICONS.superuser;
 					}
