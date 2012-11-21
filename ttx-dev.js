ac478e0a55e263c8c546ce50daebcd72784b3f28
aleontiev <alonetiev@gmail.com>
2012-11-20 20:15:36 -0500

    Update ttx-dev.js

--- ttx-dev.js
+++ ttx-dev.js
@@ -995,7 +995,7 @@ window.TTX = null;
 	    	$('.ttx-panel').each(function(){
 			$(this).appendTo(panels);
 		});
-	    	$('#ttx-panels').sortable({placeholder:'placeholder',axis:'x',handle:'.right-panel-tab',tolerance:'pointer',appendTo:document.body});
+	    	$('#ttx-panels').sortable({placeholder:'placeholder',handle:'.right-panel-tab',appendTo:'parent'});
 	    }
 	    if (typeof _panels === 'undefined'){ // build the panels object
 		_panels = { dock: [], float: [], hidden: [] };
