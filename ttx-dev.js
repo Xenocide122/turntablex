37f5c29e7ef0ce415e980e8fa0c4413952537f5c
aleontiev <alonetiev@gmail.com>
2012-11-21 15:02:42 -0500

    Update ttx-dev.js

--- ttx-dev.js
+++ ttx-dev.js
@@ -998,7 +998,7 @@ window.TTX = null;
 		
 	    
 	    	$('.ttx-panel').not('#ttx-panels-scene').draggable({handle:'.right-panel-tab',revert:'invalid',revertDuration:'100ms'});
-		$('.ttx-panel').droppable({tolerance:'pointer',accept:'.ttx-panel'});
+		$('.ttx-panel').droppable({tolerance:'intersect',accept:'.ttx-panel'});
 	
 	    }
 	    if (typeof _panels === 'undefined'){ // build the panels object
