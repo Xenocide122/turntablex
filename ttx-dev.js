ffe602b52f6f34831364c1b23dd5d9856eabbe10
aleontiev <alonetiev@gmail.com>
2012-11-28 01:36:29 -0500

    Update ttx-dev.js

--- ttx-dev.js
+++ ttx-dev.js
@@ -793,10 +793,7 @@ window.TTX = null;
 		$('#scene').css({width:'1468px',height:'100%',left:'auto',right:'50%',top:'50%',marginTop:'-300px',marginLeft:'0px',marginRight:'-734px'})
 	}
 	function onPanelStop(event,ui){
-		$(this).sortable("destroy");
-		if (ui.helper.css('height') !== '100%'){
-			
-		}
+		log(ui);
 	}
 	function onPanelMove(event,ui){
 		if (ui.offset.top > 0.3 * $('#ttx-panels').height()){
@@ -1213,7 +1210,7 @@ window.TTX = null;
 		}
 	    	var dragOptions = {stack:'.ttx-panel',distance:10,handle:'.floating-panel-tab',revert:true,revertDuration:'100ms',stop:function(event,ui){	
 		}};
-		$('#ttx-panels').sortable({beforeStop:onPanelStop,sort:onPanelMove,appendTo:document.body,revert:100,placeholder:'placeholder',tolerance:'intersect',scroll:false,handle:'.floating-panel-tab',start:function(event,ui){ var width = ui.helper.width(); $(this).find('.placeholder').width(width); },stop:onPanelReorder});
+		$('#ttx-panels').sortable({stop:onPanelStop,sort:onPanelMove,appendTo:document.body,revert:100,placeholder:'placeholder',tolerance:'intersect',scroll:false,handle:'.floating-panel-tab',start:function(event,ui){ var width = ui.helper.width(); $(this).find('.placeholder').width(width); },stop:onPanelReorder});
 		$('.ttx-panel').not('#ttx-panels-scene').resizable({stop: function(event,ui){$(this).css({'height':'100%','bottom':'0px','top':'0px'});}, handles:'e',minWidth:PANEL_WIDTH}); // make these resizable
 	    	
 	    	//$('#ttx-panels').sortable({forceHelperSize:true,helper:'clone',tolerance:'pointer',zIndex:9999,handle:'.floating-panel-tab',placeholder:'placeholder'}).sortable("enable");
