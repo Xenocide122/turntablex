0724ea795900dffe44cbdeb919f9fbc578d415be
aleontiev <alonetiev@gmail.com>
2012-11-29 02:27:30 -0500

    Update ttx-dev.js

--- ttx-dev.js
+++ ttx-dev.js
@@ -820,7 +820,7 @@ window.TTX = null;
 		// push it into the dock
 		var stop = false;
 		$('#ttx-panels .ttx-panel').each(function(){
-			if (stop === false && $(this).offset().left < panel.offset().left){
+			if (stop === false && $(this).offset().left > panel.offset().left){
 				$(this).after(panel.detach());
 				stop = true;	
 			} 	
