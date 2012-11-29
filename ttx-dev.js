9c0f2d4dda1da1f2961be32d3b47d3bd0bc488b6
aleontiev <alonetiev@gmail.com>
2012-11-28 20:34:15 -0500

    Update ttx-dev.js

--- ttx-dev.js
+++ ttx-dev.js
@@ -839,7 +839,7 @@ window.TTX = null;
 	}
 	function onPanelStop(event,ui){
 		if (ui.item.parent().attr('id') !== 'ttx-panels'){ // dock -> floating
-			ui.item.css({'z-index':'',top:ui.placeholder.css('top'),left:ui.placeholder.css('left'),position:'absolute',width:ui.placeholder.width()+'px',height:'300px'}).draggable({handle:'.floating-panel-tab'}).resizable('destroy').resizable({minWidth:PANEL_WIDTH,minHeight:PANEL_WIDTH,handles:'n, e, w, s, ne, sw, se, nw'});
+			ui.item.addClass('float').css({'z-index':'',top:ui.placeholder.css('top'),left:ui.placeholder.css('left'),position:'absolute',width:ui.placeholder.width()+'px',height:'300px'}).draggable({handle:'.floating-panel-tab'}).resizable('destroy').resizable({minWidth:PANEL_WIDTH,minHeight:PANEL_WIDTH,handles:'n, e, w, s, ne, sw, se, nw'});
 			var id = ui.item.attr('id');
 			var name;
 			if (id === 'right-panel'){
