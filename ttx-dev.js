ba803f32f738c99fe334caaf61117488c2cb55c0
aleontiev <alonetiev@gmail.com>
2012-11-28 22:02:52 -0500

    Update ttx-dev.js

--- ttx-dev.js
+++ ttx-dev.js
@@ -291,8 +291,8 @@ window.TTX = null;
 			lstore.set('ttx-settings', settings);
 		} else {
 			// merge config with defaults to ensure no missing params
-			//settings = $.extend(true, {}, defaultSettings, settings);
-			settings = defaultSettings;
+			settings = $.extend(true, {}, defaultSettings, settings);
+			//settings = defaultSettings;
 			saveSettings();
 		}
 
