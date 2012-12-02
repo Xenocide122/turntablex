c71c5fdd9fd67fd60877eb56534a117a86d7f5ac
aleontiev <alonetiev@gmail.com>
2012-12-02 18:06:13 -0500

    Update ttx-dev.js

--- ttx-dev.js
+++ ttx-dev.js
@@ -546,10 +546,14 @@ window.TTX = null;
 		if ($element.hasClass('message')){
 			// add a timer
 			var now = new Date();
+			var messages = $element.parent();
+			if (messages.hasClass('pmHistory')){
+				return;
+			}
 			if ($element.find('.avatar').length > 0){
 				$('<div class="ttx-chat-timer" style="position: absolute; top: 5px; right: 5px; height:12px; z-index:5; color: #aaa; font-size: 12px">'+format_time(now)+'</div>').appendTo($element);
 			}
-			var messages = $element.parent();
+			
 			var scrollHeight = messages.prop('scrollHeight');
 			var height = messages.height();
 			if (scrollHeight - messages.scrollTop() - height < 100){
